bbba48c3d06ab3ce3719467cc2995e7da804b73e
choidabom <dabomi0305@naver.com>
2022-11-05 18:51:01 +0900

    tiny server complete

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -5,6 +5,35 @@
 #include "csapp.h"
 
 int main(void) {
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+
+  /* Extract the two arguments */
+  if ((buf = getenv("QUERY_STRING")) != NULL){ // QUERY_STRING이라는 환경변수
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p+1);  
+    n1 = atoi(arg1);    // atoi: ASCII to integer, string을 int로 바꿔줌, int를 return
+    n2 = atoi(arg2);  
+  }
+
+  /* Make the response body */
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+         content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting visiting!\r\n", content);
+
+  /* Generate the HTTP response */
+  printf("Connection: close\r\n");
+  printf("Connection-length: %d\r\n", (int)strlen(content));
+  printf("Connection-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
+  
   exit(0);
 }
 /* $end adder */
